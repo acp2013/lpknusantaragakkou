a91a5856878a6de8ca3b5d3a43e03630e486e813
Ahnan <ahnanfauzy@gmail.com>
2021-10-19 13:15:44 +0700

    Loader.py implemented

--- loader.php
+++ loader.php
@@ -15,7 +15,7 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form; 
+  $contact = new PHP_Email_Form;
   $contact->ajax =true;
 
   $contact->to = $receiving_email_address;
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
